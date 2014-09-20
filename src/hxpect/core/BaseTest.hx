515937121c0471b7a52cc1e0a8dde641082f8aa2
John Beech <github@mkv25.net>
2014-09-20 17:41:40 +0100

    Refactored BaseTest to use new exception fluid handling format.

--- src/hxpect/core/BaseTest.hx
+++ src/hxpect/core/BaseTest.hx
@@ -9,38 +9,11 @@ class BaseTest
 	
 	public function expectNoExceptions(runBlock:Void->Void):Void
 	{
-		try
-		{
-			runBlock();
-		}
-		catch (exception:Dynamic)
-		{
-			throw "Assertion failed unexpectedly: " + exception;
-		}
+		expect(runBlock).to.not.throwException();
 	}
 	
 	public function expectException(runBlock:Void->Void, expectedException:Dynamic):Void
 	{
-		var testPassed:Bool = false;
-		try
-		{
-			runBlock();
-		}
-		catch (exception:Dynamic)
-		{
-			if (exception == expectedException)
-			{
-				testPassed = true;
-			}
-			else
-			{
-				throw "Assertion failed for wrong reason: " + exception;
-			}
-		}
-		
-		if (!testPassed)
-		{
-			throw "Assertion failed to fail.";
-		}
+		expect(runBlock).to.throwException(expectedException);
 	}
 }
