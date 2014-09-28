935af33edd9e856d3485203b7a2c3e726fc9a9b7
John Beech <github@mkv25.net>
2014-09-28 12:21:42 +0100

    Added compile step to generate haxe/lib/openfl/{version}/backends/native -cp path by using "haxelib config" to figure this out.

--- runner/src/hxpect/runner/Compiler.hx
+++ runner/src/hxpect/runner/Compiler.hx
@@ -3,6 +3,7 @@ package hxpect.runner;
 import hxpect.core.Logger;
 import sys.FileSystem;
 import sys.io.File;
+import sys.io.Process;
 
 class Compiler
 {
@@ -113,19 +114,24 @@ class Compiler
 		
 		var hxpectLib:String = (flagIncludeHxpectLib) ? "-lib hxpect " + NL : "";
 		
+		var openflNativeBackendsLib = "-cp " + readHaxelibPathFor("openfl") + "/backends/native";
+		
 		var fileContents = ""
 			+ "-cp " + binarySourcePath + NL
 			+ "-cp " + sourcePath + " " + NL
 			+ "-cp " + testPath + " " + NL
 			+ NL
 			+ "-main hxpect.TempTestRunner " + NL
+			+ "-neko " + outputFilePath + NL
 			+ NL
 			+ hxpectLib
 			+ "-lib openfl " + NL
-			+ "--remap flash:openfl " + NL
+			+ "-lib lime " + NL
 			+ "-lib actuate " + NL
-			+ "-neko " + outputFilePath;
-		
+			+ openflNativeBackendsLib + NL
+			+ NL
+			+ "--remap flash:openfl " + NL;
+			
 		logger.logInfo("HXML File Contents:");
 		logger.logInfo(fileContents);
 		logger.logEmptyLine();
@@ -133,14 +139,50 @@ class Compiler
 		writeStringToFile(fileContents, hxmlFilePath);
 	}
 	
+	function readHaxelibPathFor(libraryName:String):String
+	{
+		var commandResult = runCommand("haxelib", ["config"]);
+		
+		var haxelibPath = commandResult.result;
+		var libraryDevPath = readFileContents(haxelibPath + libraryName + "/.dev");
+		
+		if (libraryDevPath != null)
+		{
+			return libraryDevPath;
+		}
+		else
+		{
+			var libraryCurrent = readFileContents(haxelibPath + libraryName + "/.current");
+			var libraryFolder = StringTools.replace(libraryCurrent, ".", ",");
+			var librayPath = haxelibPath + libraryName + "/" + libraryFolder;
+			
+			return librayPath;
+		}
+	}
+	
+	function readFileContents(filePath:String):String
+	{
+		if (FileSystem.exists(filePath))
+		{
+			var file = File.read(filePath);
+			var contents = file.readAll().toString();
+			file.close();
+			
+			return contents;
+		}
+		return null;
+	}
+	
 	function compile():Void
 	{
 		logger.log("Compiling test runner: " + outputFilePath);
-		var compileResult = runCommand("haxe " + hxmlFilePath, additionalArgs);
+		
+		var compileResult = runCommand("haxe " + hxmlFilePath);
 		if (!compileResult.ok())
 		{
 			Sys.exit(compileResult.code);
 		}
+		
 		logger.logEmptyLine();
 	}
 	
@@ -160,11 +202,52 @@ class Compiler
 	
 	function runCommand(command:String, ?args:Array<String>):CommandResult
 	{
-		var argsString = (args == null) ? "" : " " + args.join(" ");
-		logger.logInfo(command + argsString);
-		var resultCode = Sys.command(command, args);
+		var sanitizedCommand = command;
+		var sanitizedArgs = new Array<String>();
 		
-		return new CommandResult(resultCode);
+		// move args that exist in the command string into an args array
+		var commandArgs = command.split(" ");
+		if (commandArgs.length > 0)
+		{
+			sanitizedCommand = commandArgs[0];
+			for (i in 1...commandArgs.length)
+			{
+				var commandArg = commandArgs[i];
+				sanitizedArgs.push(commandArg);
+			}
+		}
+		
+		// stitch additional arts onto the end of the command args
+		if (args != null)
+		{
+			for (arg in args)
+			{
+				sanitizedArgs.push(arg);
+			}
+		}
+		
+		// report about this action
+		logger.logInfo(sanitizedCommand + sanitizedArgs.join(" "));
+		
+		// do the thing, record the results
+		var process = new Process(sanitizedCommand, sanitizedArgs);
+		var exitCode = process.exitCode();
+		var result = prettify(process.stdout.readAll().toString());
+		var error = prettify(process.stderr.readAll().toString());
+		process.close();
+		
+		// report about the result
+		logger.log(result);
+		
+		return new CommandResult(exitCode, result, error);
+	}
+	
+	function prettify(string:String):String
+	{
+		string = StringTools.trim(string);
+		string = StringTools.replace(string, "\r", "");
+		
+		return string;
 	}
 	
 	function listFiles(directory:String, ?appendTo:Array<String>):Array<String>
@@ -200,15 +283,29 @@ class Compiler
 class CommandResult
 {
 	public var code(get, null):Int;
+	public var result(get, null):String;
+	public var error(get, null):String;
 	
-	public function new(code:Int)
+	public function new(code:Int, result:String, error:String)
 	{
 		this.code = code;
+		this.result = result;
+		this.error = error;
 	}
 	
 	function get_code():Int
 	{
 		return this.code;
+	}
+	
+	function get_result():String
+	{
+		return this.result;
+	}
+	
+	function get_error():String
+	{
+		return this.error;
 	}
 	
 	public function ok():Bool
